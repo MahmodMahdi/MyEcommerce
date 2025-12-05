6926f4cfff4631d9c1c52d7c83af0db84c7c76be
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-05 23:04:39 +0200

    Add AppDbContext class

---
+++ MyEcommerce.DataAccessLayer/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyEcommerce.DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEcommerce.DataAccessLayer.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Category> Categories { get; set; }
+	}
+}
