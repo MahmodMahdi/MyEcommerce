182b428dbe9507349217ef5cf237a50e1724fd78
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-06 12:14:22 +0200

    Add Product Repo and Interface

---
+++ MyEcommerce.DataAccessLayer/Repositories/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using MyEcommerce.DataAccessLayer.Data;
+using MyEcommerce.DomainLayer.Interfaces;
+using MyEcommerce.DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEcommerce.DataAccessLayer.Repositories
+{
+	public class ProductRepository:GenericRepository<Product>, IProductRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ProductRepository(ApplicationDbContext context) : base(context)
+		{
+			_context = context;
+		}
+
+		public void Update(Product product)
+		{
+			var productItem = _context.Products.FirstOrDefault(x=>x.Id == product.Id);
+			if (productItem != null)
+			{
+				productItem.Name = product.Name;
+				productItem.Description = product.Description;
+				productItem.Price = product.Price;
+				productItem.Image = product.Image;
+				productItem.CreatedAt = DateTime.Now;
+			}
+		}
+	}
+}
---
+++ MyEcommerce.DomainLayer/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyEcommerce.DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEcommerce.DomainLayer.Interfaces
+{
+	public interface IProductRepository : IGenericRepository<Product>
+	{
+		void Update(Product product);
+	}
+}
