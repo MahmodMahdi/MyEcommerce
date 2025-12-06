24a9a952d18964447c55f8836c28ff537706ac0d
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-06 10:41:24 +0200

    Add UnitOfWork

---
+++ MyEcommerce.DomainLayer/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEcommerce.DomainLayer.Interfaces
+{
+	public interface IUnitOfWork:IDisposable
+	{
+		ICategoryRepository CategoryRepository { get; }
+		int complete();
+	}
+}
