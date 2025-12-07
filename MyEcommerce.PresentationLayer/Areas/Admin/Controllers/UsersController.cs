828572383d8e61136d615b9a0cf8ef8685c9b423
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-07 17:04:07 +0200

    Add User Index EndPoint for admin Controll

---
+++ MyEcommerce.PresentationLayer/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyEcommerce.DataAccessLayer.Data;
+using System.Security.Claims;
+using Utilities;
+
+namespace MyEcommerce.PresentationLayer.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles ="Admin")]
+	public class UsersController : Controller
+	{
+
+		private readonly ApplicationDbContext _context;
+		public UsersController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var claimsIdentity =(ClaimsIdentity)User.Identity; // here i want to bring claims
+			var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier); // here i filter claim of users
+			string userId = claim.Value; // here i bring user by his id
+			var Users = _context.ApplicationUsers.Where(u=>u.Id!= userId).ToList(); // here i bring all users except me(because i am admin)
+			return View(Users);
+		}
+	}
+}
