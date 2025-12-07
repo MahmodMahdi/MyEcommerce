7fac28de6b9f079114314597034cea3512cc77ab
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-07 18:44:17 +0200

    Add Utiilities Class for Helper classes

---
+++ Utilities/RoleNames.cs
@@ -0,0 +1,9 @@
+﻿namespace Utilities
+{
+	public static class RoleNames
+	{
+		public static string AdminRole = "Admin";
+		public static string EditorRole = "Editor";
+		public static string CustomerRole = "Customer";
+	}
+}
