ac468605957c77c5a3676c4a267f9864044239c5
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-08 19:53:44 +0200

    Shopping Carts migration

--- MyEcommerce.DataAccessLayer/Migrations/20251208175144_AddShoppingCart.Designer.cs
+++ MyEcommerce.DataAccessLayer/Migrations/20251208175144_AddShoppingCart.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyEcommerce.DataAccessLayer.Data;
 
@@ -11,9 +12,11 @@ using MyEcommerce.DataAccessLayer.Data;
 namespace MyEcommerce.DataAccessLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251208175144_AddShoppingCart")]
+    partial class AddShoppingCart
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
