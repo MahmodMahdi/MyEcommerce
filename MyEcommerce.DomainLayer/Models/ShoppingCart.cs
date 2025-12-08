1bf2bff4a4592cd4bebeaa0ddfdecdd590571080
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-08 19:54:13 +0200

    Update Shopping Cart model

---
+++ MyEcommerce.DomainLayer/Models/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using MyEcommerce.DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MyEcommerce.DomainLayer.Models
+{
+	public class ShoppingCart
+	{
+		public int Id { get; set; }
+		public int ProdcutId { get; set; }
+
+		[Range(1,100,ErrorMessage ="you must enter value between 1 to 100")]
+		public int Count { get; set; }
+		public string ApplicationUserId { get; set; }
+
+		[ValidateNever]
+		[ForeignKey(nameof(ProdcutId))]
+		public Product Product { get; set; }
+
+		[ValidateNever]
+		[ForeignKey(nameof(ApplicationUserId))]
+		public ApplicationUser ApplicationUser { get; set; }
+
+	}
+}
