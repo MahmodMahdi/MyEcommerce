9e3ae51004c30eb09ca12717b29fe20be8f1a6e3
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-08 19:52:58 +0200

    Add Shopping Cart to DB

--- MyEcommerce.DataAccessLayer/Data/ApplicationDbContext.cs
+++ MyEcommerce.DataAccessLayer/Data/ApplicationDbContext.cs
@@ -17,5 +17,6 @@ namespace MyEcommerce.DataAccessLayer.Data
 		public DbSet<Category> Categories { get; set; }
 		public DbSet<Product> Products { get; set; }
 		public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+		public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 	}
 }
