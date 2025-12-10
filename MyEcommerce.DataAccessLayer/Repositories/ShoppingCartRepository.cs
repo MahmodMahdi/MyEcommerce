64451944660e198b22671235bf09334cc60b86b7
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-10 09:24:37 +0200

    Add Increase and Decrease Methods to Shopping Cart Repository

---
+++ MyEcommerce.DataAccessLayer/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,31 @@
+﻿using MyEcommerce.DataAccessLayer.Data;
+using MyEcommerce.DomainLayer.Interfaces;
+using MyEcommerce.DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEcommerce.DataAccessLayer.Repositories
+{
+	public class ShoppingCartRepository : GenericRepository<ShoppingCart>, IShoppingCartRepository
+	{
+		private readonly ApplicationDbContext _context;
+		public ShoppingCartRepository(ApplicationDbContext context) : base(context)
+		{
+			_context = context;
+		}
+		public int IncreaseCount(ShoppingCart shoppingCart, int count)
+		{
+			shoppingCart.Count += count;
+			return shoppingCart.Count;
+		}
+
+		public int DecreaseCount(ShoppingCart shoppingCart, int count)
+		{
+			shoppingCart.Count -= count;
+			return shoppingCart.Count;
+		}
+
+		
+	}
+}
