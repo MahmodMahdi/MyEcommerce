76d0a1e9f24accd23f31dbc25a088a6b9163f8e7
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-10 10:51:25 +0200

    Create Order Detail Repository

---
+++ MyEcommerce.DataAccessLayer/Repositories/Order/OrderDetailRepository.cs
@@ -0,0 +1,23 @@
+﻿using MyEcommerce.DataAccessLayer.Data;
+using MyEcommerce.DomainLayer.Interfaces.Order;
+using MyEcommerce.DomainLayer.Models.Order;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEcommerce.DataAccessLayer.Repositories.Order
+{
+	public class OrderDetailRepository : GenericRepository<OrderDetail>, IOrderDetailRepository
+	{
+		private readonly ApplicationDbContext _context;
+		public OrderDetailRepository(ApplicationDbContext context) : base(context)
+		{
+			_context = context;
+		}
+		public void Update(OrderDetail orderDetail)
+		{
+			_context.OrderDetails.Update(orderDetail);
+		}
+
+	}
+}
