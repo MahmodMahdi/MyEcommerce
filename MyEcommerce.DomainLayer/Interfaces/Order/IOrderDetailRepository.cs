e8397ee8607ec9ff283e3aff17f0e064cef38004
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-10 10:52:18 +0200

    Create Order Detail Interface

---
+++ MyEcommerce.DomainLayer/Interfaces/Order/IOrderDetailRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyEcommerce.DomainLayer.Models.Order;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEcommerce.DomainLayer.Interfaces.Order
+{
+	public interface IOrderDetailRepository:IGenericRepository<OrderDetail>
+	{
+		void Update(OrderDetail orderDetail);
+	}
+}
