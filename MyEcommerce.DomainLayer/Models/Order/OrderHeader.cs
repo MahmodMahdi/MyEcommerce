afaac82e0241f414e997d8953c84f7738a4d9f9d
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-10 10:50:26 +0200

    Create Order Header Model

---
+++ MyEcommerce.DomainLayer/Models/Order/OrderHeader.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEcommerce.DomainLayer.Models.Order
+{
+	public class OrderHeader
+	{
+		public int Id { get; set; }
+		public DateTime OrderDate { get; set; }
+		public DateTime ShippingDate { get; set; }
+		public decimal TotalPrice { get; set; }
+		public string? OrderStatus { get; set; }
+		public string? PaymentStatus { get; set; }
+		public string? TrackingNumber { get; set; }
+		public string? Carrior { get; set; }
+		public DateTime PaymentDate { get; set; }
+
+		// Props of Stripe
+		public string? SissionId { get; set; }
+		public string? PaymentIntentId { get; set; }
+
+		// Date of User 
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string PhoneNumber { get; set; }
+
+		public string ApplicationUserId { get; set; }
+		[ValidateNever]
+		public ApplicationUser ApplicationUser { get; set; }
+	}
+}
