410f37affc2b3dfd0bbd58926a98662db87e0e96
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-10 10:52:49 +0200

    Create Order Header Interface

---
+++ MyEcommerce.DomainLayer/Interfaces/Order/IOrderHeaderRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyEcommerce.DomainLayer.Models.Order;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEcommerce.DomainLayer.Interfaces.Order
+{
+	public interface IOrderHeaderRepository : IGenericRepository<OrderHeader>
+	{
+		void Update(OrderHeader orderHeader);
+		void UpdateOrderStatus (int id, string OrderStatus,string PaymentStatus);
+	}
+}
