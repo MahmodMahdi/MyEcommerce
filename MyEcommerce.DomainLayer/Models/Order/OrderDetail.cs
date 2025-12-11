57fc07318e25e605159e6b6e9ef540b3bc23b65a
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-11 15:51:33 +0200

    Add foreign key of Product and OrderHeader Navegation Prop

--- MyEcommerce.DomainLayer/Models/Order/OrderDetail.cs
+++ MyEcommerce.DomainLayer/Models/Order/OrderDetail.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace MyEcommerce.DomainLayer.Models.Order
@@ -12,10 +13,12 @@ namespace MyEcommerce.DomainLayer.Models.Order
 		public int Count { get; set; }
 
 		public int ProductId { get; set; }
+		[ForeignKey(nameof(ProductId))]
 		[ValidateNever]
 		public Product Product { get; set; }
 
 		public int OrderId { get; set; }
+		[ForeignKey(nameof(OrderId))]
 		[ValidateNever]
 		public OrderHeader OrderHeader { get; set; }
 	}
