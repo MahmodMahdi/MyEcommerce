5c56f79630afd74d51f91d35d7262be67c9315b1
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-16 17:42:13 +0200

    Uneffictive Changes

--- MyEcommerce.PresentationLayer/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ MyEcommerce.PresentationLayer/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace MyEcommerce.PresentationLayer.Areas.Identity.Pages.Account.Manage
 {
     public class IndexModel : PageModel
