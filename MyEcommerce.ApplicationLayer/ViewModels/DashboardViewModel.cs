5a5addc2e60a21b70caa7f98682fbba2372f96b3
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-23 23:41:33 +0200

    feat(app): add DashboardViewModel to encapsulate sytem-wide statistics

---
+++ MyEcommerce.ApplicationLayer/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MyEcommerce.ApplicationLayer.ViewModels
+{
+	public class DashboardViewModel
+	{
+		public int TotalOrders { get; set; }
+		public int TotalCategories { get; set; }
+		public int TotalUsers { get; set; }
+		public int TotalProducts { get; set; }
+		public int ApprovedOrders { get; set; }
+		public int ShippedOrders { get; set; }
+		public int PendingOrders { get; set; }
+		public int UsersLockedAccount { get; set; }
+	    public string MostExistProduct { get; set; }
+		public string LessExistProduct { get; set; }
+		public string MostPurchasedProduct { get; set; }
+		public string MostPurchasedBuyer { get; set; }
+	}
+}
