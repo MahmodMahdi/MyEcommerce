04b62a784a10eeb6b82e3014bff0c28859c186ee
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-23 23:40:21 +0200

    refactor: decouple UI from Domain by moving ViewModels to ApplicationLayer

---
+++ MyEcommerce.ApplicationLayer/ViewModels/OrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MyEcommerce.DomainLayer.Models.Order;
+
+namespace MyEcommerce.ApplicationLayer.ViewModels
+{
+	public class OrderViewModel
+	{
+		public OrderHeader OrderHeader { get; set; }
+		public IEnumerable<OrderDetail> OrderDetails { get; set; }
+
+	}
+}
---
+++ MyEcommerce.ApplicationLayer/ViewModels/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MyEcommerce.ApplicationLayer.ViewModels
+{
+	public class UserViewModel
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+
+		public string PhoneNumber { get; set; }
+		public DateTimeOffset? LockoutEnd { get; set; } 
+	}
+}
