64ac8ca376ce3b863ac39a86606b6e7982f1fef0
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-24 20:38:29 +0200

    refactor(Data): Explicity configure Discriminator in OnModelCreating

--- MyEcommerce.DataAccessLayer/Data/ApplicationDbContext.cs
+++ MyEcommerce.DataAccessLayer/Data/ApplicationDbContext.cs
@@ -9,6 +9,20 @@ namespace MyEcommerce.DataAccessLayer.Data
 	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder); // مهم جداً عشان الـ Identity تشتغل
+
+			// تعريف الـ Discriminator برمجياً
+			builder.Entity<ApplicationUser>()
+				.HasDiscriminator<string>("Discriminator")
+				.HasValue("ApplicationUser");
+
+			// جعل القيمة الافتراضية "ApplicationUser" على مستوى قاعدة البيانات
+			builder.Entity<ApplicationUser>()
+				.Property("Discriminator")
+				.HasDefaultValue("ApplicationUser");
+		}
 		public DbSet<Category> Categories { get; set; }
 		public DbSet<Product> Products { get; set; }
 		public DbSet<ApplicationUser> ApplicationUsers { get; set; }
