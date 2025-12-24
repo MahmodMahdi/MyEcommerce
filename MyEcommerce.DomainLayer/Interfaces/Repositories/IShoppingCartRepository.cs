6448b8b4c8674b732cb3c86717ede6a593a5b5d4
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-24 21:05:27 +0200

    refactor: move IShoppingCartRepo to scope folder

---
+++ MyEcommerce.DomainLayer/Interfaces/Repositories/IShoppingCartRepository.cs
@@ -0,0 +1,10 @@
+﻿using MyEcommerce.DomainLayer.Models;
+
+namespace MyEcommerce.DomainLayer.Interfaces.Repositories
+{
+	public interface IShoppingCartRepository:IGenericRepository<ShoppingCart>
+	{
+		int IncreaseCount (ShoppingCart shoppingCart,int count);
+		int DecreaseCount (ShoppingCart shoppingCart,int count);
+	}
+}
