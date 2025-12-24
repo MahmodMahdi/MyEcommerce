7483a7befc72f0aa311577eaa8d78e650e915dc2
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-24 21:02:44 +0200

    feat(UnitOfWork): Implement Transaction for enhance code

--- MyEcommerce.DataAccessLayer/Repositories/UnitOfWork.cs
+++ MyEcommerce.DataAccessLayer/Repositories/UnitOfWork.cs
@@ -1,13 +1,15 @@
-﻿using MyEcommerce.DataAccessLayer.Data;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using MyEcommerce.DataAccessLayer.Data;
 using MyEcommerce.DataAccessLayer.Repositories.Order;
-using MyEcommerce.DomainLayer.Interfaces;
-using MyEcommerce.DomainLayer.Interfaces.Order;
+using MyEcommerce.DomainLayer.Interfaces.Repositories;
+using MyEcommerce.DomainLayer.Interfaces.Repositories.Order;
 
 namespace MyEcommerce.DataAccessLayer.Repositories
 {
 	public class UnitOfWork : IUnitOfWork
 	{
 		private readonly ApplicationDbContext _context;
+		private  IDbContextTransaction _transaction;
 		public ICategoryRepository CategoryRepository { get; private set; }
 		public IProductRepository ProductRepository { get; private set; }
 		public IShoppingCartRepository ShoppingCartRepository { get; private set; }
@@ -23,8 +25,7 @@ namespace MyEcommerce.DataAccessLayer.Repositories
 			ShoppingCartRepository = new ShoppingCartRepository(context);
 			OrderHeaderRepository = new OrderHeaderRepository(context);
 			OrderDetailRepository = new OrderDetailRepository(context);
-			ApplicationUserRepository = new ApplicationUserRepository(context);
-		}
+			ApplicationUserRepository = new ApplicationUserRepository(context);		}
 		public async Task<int> CompleteAsync()
 		{
 			return await _context.SaveChangesAsync();
@@ -32,6 +33,51 @@ namespace MyEcommerce.DataAccessLayer.Repositories
 		public void Dispose()
 		{
 			 _context?.Dispose();
+			_transaction?.Dispose(); // تأكد من التخلص من الترانزاكشن أيضاً
+		}
+
+		public async Task BeginTransactionAsync()
+		{
+			_transaction = await _context.Database.BeginTransactionAsync();
+		}
+
+		public async Task CommitTransactionAsync()
+		{
+			try
+			{
+				if (_transaction != null)
+				{
+					await _transaction.CommitAsync();
+				}
+			}
+			finally
+			{
+				// نضمن دائماً التخلص من الكائن بعد الانتهاء
+				if (_transaction != null)
+				{
+					await _transaction.DisposeAsync();
+					_transaction = null;
+				}
+			}
+		}
+
+		public async Task RollbackTransactionAsync()
+		{
+			try
+			{
+				if (_transaction != null)
+				{
+					await _transaction.RollbackAsync();
+				}
+			}
+			finally
+			{
+				if (_transaction != null)
+				{
+					await _transaction.DisposeAsync();
+					_transaction = null;
+				}
+			}
 		}
 	}
 }
---
+++ MyEcommerce.DomainLayer/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using MyEcommerce.DomainLayer.Interfaces.Repositories.Order;
+
+namespace MyEcommerce.DomainLayer.Interfaces.Repositories
+{
+	public interface IUnitOfWork:IDisposable
+	{
+		ICategoryRepository CategoryRepository { get; }
+		IProductRepository ProductRepository { get; }
+		IShoppingCartRepository ShoppingCartRepository { get; }
+		IOrderHeaderRepository OrderHeaderRepository { get; }
+		IOrderDetailRepository OrderDetailRepository { get; }
+		IApplicationUserRepository ApplicationUserRepository { get; }
+		Task BeginTransactionAsync();
+		Task CommitTransactionAsync();
+		Task RollbackTransactionAsync();
+		Task<int> CompleteAsync();
+	}
+}
