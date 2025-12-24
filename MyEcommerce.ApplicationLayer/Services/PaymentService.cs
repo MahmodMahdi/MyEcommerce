d0e56fbd2c46d99f113e90295eafdbf5d5d5c990
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-24 20:28:37 +0200

    feat: Implement Validation for Network Issue for Payment Processing

--- MyEcommerce.ApplicationLayer/Services/PaymentService.cs
+++ MyEcommerce.ApplicationLayer/Services/PaymentService.cs
@@ -52,11 +52,16 @@ namespace MyEcommerce.ApplicationLayer.Services
 				_logger.LogError(ex, "[STRIPE ERROR] Failed to create checkout session for Order #{OrderId}. Error Code: {StripeCode}", model.OrderHeader.Id, ex.Message);
 				throw;
 			}
-			catch (Exception ex)
+			catch (HttpRequestException ex)
 			{
 				// أخطاء عامة (مثل مشكلة في الإنترنت)
-				_logger.LogError(ex, "[FATAL ERROR] Unexpected failure in PaymentService for Order #{OrderId}", model.OrderHeader.Id);
-				throw;
+				_logger.LogCritical("Network Error: Could not reach Stripe servers. Check internet connection.");
+				throw new Exception("Service is temporarily unavailable due to network issues.");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "[UNEXPECTED ERROR] PaymentService failed for Order #{OrderId}", model.OrderHeader.Id);
+				throw new Exception("An unexpected error occurred while processing your payment. Please try again later.");
 			}
 		}
 		
