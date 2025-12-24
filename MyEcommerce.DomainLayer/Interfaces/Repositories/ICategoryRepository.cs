e4f39238820ff573ce07f3599c5b522e1dc67002
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-24 19:06:22 +0200

    fix(Category): Depend on Update in Generic Repository instead on creating update method in Category Repo

---
+++ MyEcommerce.DomainLayer/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using MyEcommerce.DomainLayer.Models;
+
+namespace MyEcommerce.DomainLayer.Interfaces.Repositories
+{
+	public interface ICategoryRepository: IGenericRepository<Category>
+	{
+		void Update(Category category);
+	}
+}
