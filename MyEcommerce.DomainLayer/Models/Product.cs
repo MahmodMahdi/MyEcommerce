f697e0dc921fa2c55122960360dfa939de7d3a46
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-24 21:08:53 +0200

    fix(Product): Implement Data Annotation with View Model instead of Entity

--- MyEcommerce.DomainLayer/Models/Product.cs
+++ MyEcommerce.DomainLayer/Models/Product.cs
@@ -1,28 +1,14 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-
-namespace MyEcommerce.DomainLayer.Models
+﻿namespace MyEcommerce.DomainLayer.Models
 {
 	public class Product
 	{
 		public int Id { get; set; }
-		[Required(ErrorMessage = "*")]
-		[MaxLength(30, ErrorMessage = "Name must be less than 29 letters"), MinLength(2, ErrorMessage = "Name must be greater than 2 letters.")]
 		public string Name { get; set; }
 		public string Description { get; set; }
-		[Required]
-		[Range(0, int.MaxValue, ErrorMessage = "Stock quantity cannot be negative")]
 		public int StockQuantity { get; set; }
-		[ValidateNever]
 		public string Image {  get; set; }
-		[Required(ErrorMessage ="*")]
-		[Range(1,100000,ErrorMessage ="Price must be between 1 to 100000")]
 		public decimal Price { get; set; }
-		[DisplayName("Category")]
-		[Required(ErrorMessage = "*")]
 		public int CategoryId { get; set; }
-		[ValidateNever]
 		public Category Category { get; set; }
 	}
 }
