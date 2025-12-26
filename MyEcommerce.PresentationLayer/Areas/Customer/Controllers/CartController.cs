7d505af110a8173232e63c1a97059dc36466d14a
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-26 16:28:34 +0200

    refactor: migrate Cart logic from controller to service layer for better encapsulation

--- MyEcommerce.PresentationLayer/Areas/Customer/Controllers/CartController.cs
+++ MyEcommerce.PresentationLayer/Areas/Customer/Controllers/CartController.cs
@@ -1,23 +1,20 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using MyEcommerce.DomainLayer.Interfaces;
-using MyEcommerce.DomainLayer.Models;
-using MyEcommerce.DomainLayer.Models.Order;
-using MyEcommerce.DomainLayer.ViewModels;
-using Stripe.Checkout;
+using MyEcommerce.ApplicationLayer.ViewModels;
 using System.Security.Claims;
 using Utilities;
+using MyEcommerce.ApplicationLayer.Interfaces.Services;
 
 namespace MyEcommerce.PresentationLayer.Areas.Customer.Controllers
 {
 	[Area("Customer")]
 	public class CartController : Controller
 	{
-		private readonly IUnitOfWork _UnitOfWork;
+		private readonly IShoppingCartService _shoppingCartService;
 		public int TotalCart { get; set; }
-		public CartController(IUnitOfWork unitOfWork)
+		public CartController(IShoppingCartService shoppingCartService)
 		{
-			_UnitOfWork = unitOfWork;
+			_shoppingCartService = shoppingCartService;
 		}
 		[Authorize]
 		public async Task<IActionResult> Index()
@@ -25,260 +22,103 @@ namespace MyEcommerce.PresentationLayer.Areas.Customer.Controllers
 			// here i need user who login
 			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-			var shoppingCartViewModel = new ShoppingCartViewModel()
-			{
-				Carts = await _UnitOfWork.ShoppingCartRepository.GetAllAsync(u => u.ApplicationUserId == userId, IncludeProperties: "Product"),
-				OrderHeader = new()
-
-			};
+			var shoppingCartViewModel = await _shoppingCartService.GetAllAsync(userId);
 			HttpContext.Session.SetInt32(Helper.SessionKey, shoppingCartViewModel.Carts.Count());
-			// here i want to sum the carts which user order
-			shoppingCartViewModel.TotalCarts = shoppingCartViewModel.Carts.Sum(c => c.Count * c.Product.Price);
-
 			return View(shoppingCartViewModel);
 		}
+		[Authorize]
 		[HttpGet]
 		public async Task<IActionResult> Summary()
 		{
 			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-			var shoppingCartViewModel = new ShoppingCartViewModel()
+			var shoppingCartVM = await _shoppingCartService.GetSummaryAsync(userId);
+			if (shoppingCartVM.Carts == null || !shoppingCartVM.Carts.Any())
 			{
-				Carts = await _UnitOfWork.ShoppingCartRepository.GetAllAsync(u => u.ApplicationUserId == userId, IncludeProperties: "Product"),
-				OrderHeader = new()
-
-			};
-			// --- منطق التطهير الذكي ---
-			bool isChanged = false;
-			foreach (var cart in shoppingCartViewModel.Carts.ToList()) // ToList عشان م يحصلش Error وقت الحذف
-			{
-				// 1. لو المنتج خلص تماماً والمستخدم لسه عنده منه في السلة
-				if (cart.Product.StockQuantity <= 0)
-				{
-					await _UnitOfWork.ShoppingCartRepository.RemoveAsync(cart);
-					isChanged = true;
-				}
-				// 2. لو المخزن قل وبقى أقل من اللي اليوزر حاطه في السلة
-				else if (cart.Count > cart.Product.StockQuantity)
-				{
-					cart.Count = cart.Product.StockQuantity; // بنعدل الكمية لتناسب المتاح حالياً
-					isChanged = true;
-				}
+				TempData["Error"] = "Your cart is empty or items are no longer available.";
+				return RedirectToAction(nameof(Index));
 			}
-
-			if (isChanged)
-			{
-				await _UnitOfWork.CompleteAsync();
-				var actualCartCount = (await _UnitOfWork.ShoppingCartRepository.GetAllAsync(u => u.ApplicationUserId == userId)).Count();
-				HttpContext.Session.SetInt32(Helper.SessionKey, actualCartCount);
-				TempData["Error"] = "Some items in your cart were updated or removed due to limited stock.";
-				// نعيد جلب البيانات بعد التحديث عشان الحسابات تطلع صح
-				shoppingCartViewModel.Carts = await _UnitOfWork.ShoppingCartRepository.GetAllAsync(u => u.ApplicationUserId == userId, IncludeProperties: "Product");
-			}
-			// --- نهاية منطق التطهير ---
-			// this data that appear when opening the page (Customer info)
-			shoppingCartViewModel.OrderHeader.ApplicationUser = await _UnitOfWork.ApplicationUserRepository.GetByIdAsync(x => x.Id == userId);
-			shoppingCartViewModel.OrderHeader.Name = shoppingCartViewModel.OrderHeader.ApplicationUser.Name;
-			shoppingCartViewModel.OrderHeader.Address = shoppingCartViewModel.OrderHeader.ApplicationUser.Address;
-			shoppingCartViewModel.OrderHeader.City = shoppingCartViewModel.OrderHeader.ApplicationUser.City;
-			shoppingCartViewModel.OrderHeader.PhoneNumber = shoppingCartViewModel.OrderHeader.ApplicationUser.PhoneNumber;
-
-			shoppingCartViewModel.OrderHeader.TotalPrice = shoppingCartViewModel.Carts.Sum(c => c.Count * c.Product.Price);
-
-			return View(shoppingCartViewModel);
+			HttpContext.Session.SetInt32(Helper.SessionKey, shoppingCartVM.Carts.Count());
+			return View(shoppingCartVM);
 
 		}
+		[Authorize]
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		public async Task<IActionResult> PostSummary(ShoppingCartViewModel shoppingCartViewModel)
 		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			var domain = $"{Request.Scheme}://{Request.Host.Value}/";
+			//var domain = "https://localhost:7148/"; // manual
 			try
 			{
-				// get the user 
-				var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-				// get the carts of that user assigned
-				shoppingCartViewModel.Carts = await _UnitOfWork.ShoppingCartRepository.GetAllAsync(u => u.ApplicationUserId == userId, IncludeProperties: "Product");
-				// التحقق النهائي قبل الدفع
-				foreach (var item in shoppingCartViewModel.Carts)
-				{
-					if (item.Count > item.Product.StockQuantity)
-					{
-						TempData["Error"] = $"The item '{item.Product.Name}' is no longer available in this quantity. Only {item.Product.StockQuantity} left.";
-						return RedirectToAction(nameof(Index));
-					}
-				}
-				#region Order Header Info 
-				// set a status data info for user
-				shoppingCartViewModel.OrderHeader.OrderStatus = Helper.Pending;
-				shoppingCartViewModel.OrderHeader.PaymentStatus = Helper.Pending;
-				shoppingCartViewModel.OrderHeader.OrderDate = DateTime.Now;
-				shoppingCartViewModel.OrderHeader.ApplicationUserId = userId;
-
-				// sum count of cart total price
-				shoppingCartViewModel.OrderHeader.TotalPrice = shoppingCartViewModel.Carts.Sum(c => c.Count * c.Product.Price);
-
-				// add this data in OrderHeader in DB
-				await _UnitOfWork.OrderHeaderRepository.AddAsync(shoppingCartViewModel.OrderHeader);
-				await _UnitOfWork.CompleteAsync();
-				#endregion
-				#region Order Detail Info
-				// here it will loop in Carts and get the details of order and set it into Order Detail Model
-				foreach (var cart in shoppingCartViewModel.Carts)
-				{
-					var OrderDetail = new OrderDetail()
-					{
-						ProductId = cart.ProductId,
-						OrderId = shoppingCartViewModel.OrderHeader.Id,
-						Price = cart.Product.Price,
-						Count = cart.Count
-					};
-					// add this data in OrderDetail in DB
-					await _UnitOfWork.OrderDetailRepository.AddAsync(OrderDetail);
-				}
-				await _UnitOfWork.CompleteAsync();
-				#endregion
-				// here code of stripe method (from stripe site) but i set my value 
-				return await ProcessStripePayment(shoppingCartViewModel);
-
+				var result = await _shoppingCartService.CreateOrderAsync(shoppingCartViewModel, userId, domain);
+				Response.Headers.Append("Location", result.Url);
+				return new StatusCodeResult(303);
 			}
 			catch (Exception ex)
 			{
-				// أحياناً Stripe يرمي خطأ من نوع HttpRequestException مباشرة
-				TempData["Error"] = "Connection Failed: Please check your internet and try again.";
-
-				// سطر للـ Debugging فقط ليظهر لك في الـ Output Window ما هو الخطأ الحقيقي
-				System.Diagnostics.Debug.WriteLine($"Error Type: {ex.GetType().Name}, Message: {ex.Message}");
+				TempData["Error"] =ex.Message;
 				return RedirectToAction(nameof(Index));
 			}
 		}
+		[Authorize]
 		public async Task<IActionResult> OrderConfirmation(int id)
 		{
 			try
 			{
-				var OrderHeader = await _UnitOfWork.OrderHeaderRepository.GetByIdAsync(u => u.Id == id);
-				// check my session in stripe
-				var service = new SessionService();
-				var session = await service.GetAsync(OrderHeader.SessionId);
-				if (session.PaymentStatus.ToLower() == "paid")
+				bool isConfirmed = await _shoppingCartService.OrderConfirmation(id);
+				if (isConfirmed)
 				{
-					await _UnitOfWork.OrderHeaderRepository.UpdateOrderStatusAsync(id, Helper.Approve, Helper.Approve);
-					// when order done it will fill the paymentIntentId of Db with PII with stripe
-					OrderHeader.PaymentIntentId = session.PaymentIntentId;
-					// --- منطق خصم المخزون (Clean & Efficient) ---
-					var orderDetails = await _UnitOfWork.OrderDetailRepository.GetAllAsync(x => x.OrderId == id);
-
-					foreach (var item in orderDetails)
-					{
-						var product = await _UnitOfWork.ProductRepository.GetByIdAsync(p => p.Id == item.ProductId);
-						if (product != null)
-						{
-							product.StockQuantity -= item.Count; // خصم الكمية المباعة من المخزن
-						}
-					}
-					await _UnitOfWork.CompleteAsync();
-
-					// if status is paid then i need to remove items from cart 
-					var shoppingCart = await _UnitOfWork.ShoppingCartRepository.GetAllAsync(u => u.ApplicationUserId == OrderHeader.ApplicationUserId);
-					await _UnitOfWork.ShoppingCartRepository.RemoveRangeAsync(shoppingCart);
-					await _UnitOfWork.CompleteAsync();
 					HttpContext.Session.SetInt32(Helper.SessionKey, 0);
+					return View(id);
 				}
-				return View(id);
+				return RedirectToAction($"{nameof(Index)}");
 			}
 			catch (Exception)
 			{
-				// في حال فشل النت هنا، نوجه المستخدم لصفحة تخبره بأننا سنؤكد طلبه فور عودة الخدمة
 				TempData["Error"] = "Connection lost while confirming payment. Don't worry, your order is being processed.";
 				return RedirectToAction("Index", "Home");
 			}
 		}
-		public async Task<IActionResult> Plus(int CartId)
+		public async Task<IActionResult> Plus(int cartId)
 		{
-			var ShoppingCart = await _UnitOfWork.ShoppingCartRepository.GetByIdAsync(c => c.Id == CartId,IncludeProperties:"Product");
-			if (ShoppingCart == null) return NotFound();
-			if (ShoppingCart.Count >= ShoppingCart.Product.StockQuantity)
+			try
 			{
-				TempData["Error"] = $"Sorry, only {ShoppingCart.Product.StockQuantity} items available in stock.";
-			}
-			else
-			{
-				_UnitOfWork.ShoppingCartRepository.IncreaseCount(ShoppingCart, 1);
-				await _UnitOfWork.CompleteAsync();
-			}
-				return RedirectToAction(nameof(Index));
-		}
-		public async Task<IActionResult> Minus(int CartId)
-		{
-			var ShoppingCart = await _UnitOfWork.ShoppingCartRepository.GetByIdAsync(c => c.Id == CartId);
-			if (ShoppingCart.Count <= 1)
-			{
-				await RemoveFromCart(ShoppingCart);
+				var totalCart = await _shoppingCartService.IncrementCountAsync(cartId);
+				HttpContext.Session.SetInt32(Helper.SessionKey, totalCart);
 				return RedirectToAction(nameof(Index));
 			}
-			else
+			catch(InvalidOperationException ex)
 			{
-				_UnitOfWork.ShoppingCartRepository.DecreaseCount(ShoppingCart, 1);
+					TempData["Error"] = $"Sorry, only {ex.Message} items available in stock.";
+					return RedirectToAction(nameof(Index));
 			}
-			await _UnitOfWork.CompleteAsync();
-			return RedirectToAction(nameof(Index));
+			catch (Exception ex)
+			{
+			TempData["Error"] = "Something went wrong. Please try again";
+				return RedirectToAction(nameof (Index));
+			}
 		}
-
+		public async Task<IActionResult> Minus(int cartId)
+		{
+			try {
+				var remainingItem = await _shoppingCartService.DecrementCountAsync(cartId);
+				HttpContext.Session.SetInt32(Helper.SessionKey, remainingItem);
+				return RedirectToAction(nameof(Index));
+			}
+			catch (Exception ex)
+			{
+				TempData["Error"] = "Could not update the quantity. Please try again.";
+				return RedirectToAction(nameof(Index));
+			}
+		}
+		[Authorize]
 		public async Task<IActionResult> Remove(int cartId)
 		{
-			var ShoppingCart = await _UnitOfWork.ShoppingCartRepository.GetByIdAsync(c => c.Id == cartId);
-			await RemoveFromCart(ShoppingCart);
+			var totalCarts =  await _shoppingCartService.DeleteAsync(cartId);
+			HttpContext.Session.SetInt32(Helper.SessionKey, totalCarts);
 			return RedirectToAction(nameof(Index));
-		}
-		private async Task RemoveFromCart(ShoppingCart cart)
-		{
-			await _UnitOfWork.ShoppingCartRepository.RemoveAsync(cart);
-			await _UnitOfWork.CompleteAsync();
-
-			var count = (await _UnitOfWork.ShoppingCartRepository.GetAllAsync(s => s.ApplicationUserId == cart.ApplicationUserId)).Count();
-			HttpContext.Session.SetInt32(Helper.SessionKey, count);
-		}
-		private async Task<IActionResult> ProcessStripePayment(ShoppingCartViewModel shoppingCartViewModel)
-		{
-			#region Stripe code
-			var domain = "https://localhost:7148/"; // manual
-			var options = new SessionCreateOptions
-			{
-				LineItems = new List<SessionLineItemOptions>(),
-				// manual
-				Mode = "payment",
-				SuccessUrl = domain + $"Customer/Cart/OrderConfirmation?id={shoppingCartViewModel.OrderHeader.Id}",
-				CancelUrl = domain + $"Customer/Cart/Index",
-			};
-			// manual to set my data
-			foreach (var item in shoppingCartViewModel.Carts)
-			{
-				var sessionLineOption = new SessionLineItemOptions
-				{
-					PriceData = new SessionLineItemPriceDataOptions
-					{
-						UnitAmount = (long)(item.Product.Price * 100), // القيمة هنا عشرية وانا عايز رقم ثابت 
-						Currency = "egp",
-						ProductData = new SessionLineItemPriceDataProductDataOptions
-						{
-							Name = item.Product.Name,
-						},
-					},
-					Quantity = item.Count,
-				};
-				options.LineItems.Add(sessionLineOption);
-			}
-
-			var service = new SessionService();
-			Session session = await service.CreateAsync(options);
-			// check of session to confirm (success of not)
-			shoppingCartViewModel.OrderHeader.SessionId = session.Id;
-			await _UnitOfWork.CompleteAsync();
-			Response.Headers.Append("Location", session.Url);
-			#endregion
-
-			return new StatusCodeResult(303);
-
 		}
 	}
 }
