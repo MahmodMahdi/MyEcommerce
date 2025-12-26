cbdff973b8675fefe9f58c579c9df6da8e965cb9
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-26 16:28:08 +0200

    refactor: migrate Users logic from controller to service layer for better encapsulation

--- MyEcommerce.PresentationLayer/Areas/Admin/Controllers/UsersController.cs
+++ MyEcommerce.PresentationLayer/Areas/Admin/Controllers/UsersController.cs
@@ -1,64 +1,38 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using MyEcommerce.DataAccessLayer.Data;
+using MyEcommerce.ApplicationLayer.Interfaces.Services;
 using System.Security.Claims;
+using Utilities;
 
 namespace MyEcommerce.PresentationLayer.Areas.Admin.Controllers
 {
 	[Area("Admin")]
-	[Authorize(Roles ="Admin")]
+	[Authorize(Roles = Helper.AdminRole)]
 	public class UsersController : Controller
 	{
-
-		private readonly ApplicationDbContext _context;
-		public UsersController(ApplicationDbContext context)
+		private readonly IApplicationUserService _applicationUserService;
+		public UsersController(IApplicationUserService applicationUserService)
 		{
-			_context = context;
+			_applicationUserService = applicationUserService;
 		}
 
-		public async Task<IActionResult> Index(int pageNumber =1)
+		public async Task<IActionResult> Index(int pageNumber = 1)
 		{
 			if (pageNumber < 1) pageNumber = 1;
-
-			int pageSize = 10; // عدد المستخدمين في كل صفحة
-			
+			var pageSize = 10;
 			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-			int totalUsers = await _context.ApplicationUsers
-				.Where(u => u.Id != userId)
-				.CountAsync();
-
-			int numberOfPages = (int)Math.Ceiling((double)totalUsers / pageSize);
-			int numberOfItemToSkip = (pageNumber - 1) * pageSize;
-
-			var users = await _context.ApplicationUsers
-				.Where(u => u.Id != userId)
-				.OrderBy(u => u.UserName)
-				.Skip(numberOfItemToSkip)
-				.Take(pageSize)
-				.ToListAsync();
-			ViewBag.PageNo = pageNumber;
-			ViewBag.NoOfPages = numberOfPages;
-
-			return View(users);
+			
+			var result = await _applicationUserService.GetPaginatedUsersAsync(userId, pageNumber, pageSize);
+			return View(result);
 		}
-		public IActionResult LockUnlock(string? id)
+		public async Task<IActionResult> LockUnlock(string? id)
 		{
-			var user = _context.ApplicationUsers.FirstOrDefault(u => u.Id==id);
+			var user = await _applicationUserService.LockUnlock(id);
 			if (user == null)
 			{
 				return NotFound();
 			}
-			if (user.LockoutEnd == null || user.LockoutEnd < DateTime.Now)
-			{
-				user.LockoutEnd = DateTime.Now.AddYears(1);
-			}
-			else
-			{
-				user.LockoutEnd = DateTime.Now;
-			}
-			_context.SaveChanges();
+		
 		    return RedirectToAction("Index", "Users", new {area ="Admin"});
 		}
 	}
