20cd1327d32670d6dcd33bb98735da8459409796
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-27 17:08:12 +0200

    refactor: move service registerations to static extension mehod for cleaner program.cs

---
+++ MyEcommerce.ApplicationLayer/Extensions/DependecyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.DependencyInjection;
+using MyEcommerce.ApplicationLayer.Interfaces.Services;
+using MyEcommerce.ApplicationLayer.Services;
+using MyEcommerce.DataAccessLayer.Repositories;
+using MyEcommerce.DomainLayer.Interfaces.Repositories;
+
+namespace MyEcommerce.ApplicationLayer.Extensions
+{
+	public static class DependecyInjection
+	{
+		public static IServiceCollection AddApplicationProject(this IServiceCollection services)
+		{
+			services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+			services.AddScoped<IHomeService, HomeService>();
+			services.AddScoped<ICategoryService, CategoryService>();
+			services.AddScoped<IProductService, ProductServices>();
+			services.AddScoped<IShoppingCartService, ShoppingCartService>();
+			services.AddScoped<IOrderServices, OrderServices>();
+			services.AddScoped<IDashboardService, DashboardService>();
+			services.AddScoped<IApplicationUserService, ApplicationUserService>();
+			services.AddScoped<IPaymentService, PaymentService>();
+			services.AddTransient<IEmailService, EmailService>();
+
+			services.AddScoped<IImageService, ImageService>();
+			services.AddTransient<IEmailSender, EmailService>();
+
+			return services;
+		}
+	}
+}
