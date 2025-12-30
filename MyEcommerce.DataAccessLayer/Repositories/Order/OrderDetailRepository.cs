be14edccdb06d35b5887f5427feb83b2bf765b40
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-30 16:47:11 +0200

    feat: Update MostPurchasedProduct method to return only approved status

--- MyEcommerce.DataAccessLayer/Repositories/Order/OrderDetailRepository.cs
+++ MyEcommerce.DataAccessLayer/Repositories/Order/OrderDetailRepository.cs
@@ -2,6 +2,7 @@
 using MyEcommerce.DataAccessLayer.Data;
 using MyEcommerce.DomainLayer.Interfaces.Repositories.Order;
 using MyEcommerce.DomainLayer.Models.Order;
+using Utilities;
 
 namespace MyEcommerce.DataAccessLayer.Repositories.Order
 {
@@ -14,7 +15,7 @@ namespace MyEcommerce.DataAccessLayer.Repositories.Order
 		}
 		public void Update(OrderDetail orderDetail)
 		{
-			 _context.OrderDetails.Update(orderDetail);
+			_context.OrderDetails.Update(orderDetail);
 		}
 		public async Task<string> MostPurchasedProductAsync()
 		{
@@ -26,8 +27,9 @@ namespace MyEcommerce.DataAccessLayer.Repositories.Order
 			//return topProduct;
 
 			/// another way
-			var topProduct =await _context.OrderDetails
+			var topProduct = await _context.OrderDetails
 				.AsNoTracking()
+				.Where(x=>x.OrderHeader.OrderStatus == Helper.Approve)
 				.GroupBy(P => new { P.ProductId, P.Product.Name })
 				.Select(g => new
 				{
@@ -35,6 +37,7 @@ namespace MyEcommerce.DataAccessLayer.Repositories.Order
 					TotalSold = g.Sum(x => x.Count)
 				})
 				.OrderByDescending(x => x.TotalSold)
+				.ThenBy(x=>x.ProductName)
 				.Select(p => p.ProductName)
 				.FirstOrDefaultAsync();
 			return topProduct ?? "No Sales Yet";
