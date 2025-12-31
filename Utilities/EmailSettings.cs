d434ca0c2c934189d899cc4fd50b72db1d95c8d1
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2025-12-31 14:46:18 +0200

    feat: Last touches

--- Utilities/EmailSettings.cs
+++ Utilities/EmailSettings.cs
@@ -2,7 +2,7 @@
 {
 	public class EmailSettings
 	{
-		public int Port { get; set; }
+		public int Port { get; set; } 
 		public string? Host { get; set; }
 		public string? FromEmail { get; set; }
 		public string? Password { get; set; }
